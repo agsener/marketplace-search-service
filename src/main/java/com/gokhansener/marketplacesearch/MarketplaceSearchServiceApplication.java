d004d49f701fe8e5266b3c689ba613e1fde8b429
AGS <ahmetgokhansener@gmail.com>
2026-06-26 14:03:22 +0200

    Initialize Spring Boot marketplace search service

---
+++ src/main/java/com/gokhansener/marketplacesearch/MarketplaceSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gokhansener.marketplacesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceSearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokhansener/marketplacesearch/MarketplaceSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokhansener.marketplacesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
